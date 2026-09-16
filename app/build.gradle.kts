bc64a73f0e673e2b0a5086ba48677c7d6ba5dcd8
sami2133 <sami.akherati@gmail.com>
2026-09-16 17:11:19 +0000

    Releases 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.realestate.sami"
         minSdk = 26
         targetSdk = 34
-        versionCode = 56
-        versionName = "1.1.1"
+        versionCode = 57
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
